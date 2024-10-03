2bed3845db1edf69d94ad58d6023c733571375d2
Ammar Qammaz <ammarkov@gmail.com>
2024-10-03 15:05:27 +0300

    rearrange run script

--- scripts/linux_run.sh
+++ scripts/linux_run.sh
@@ -4,20 +4,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 cd ..
 
-
 if [ -d venv-ui/ ]
 then
-echo "Found a virtual environment" 
-source venv-ui/bin/activate
+ echo "Found a virtual environment" 
+ source venv-ui/bin/activate
+ #Run Server using LLAMA
+ python3 clean-ui.py --llama&
+ echo "Waiting for server"
+ #Wait for server to wakeup
+ sleep 5
+ #Open browser?
+ echo "Opening browser"
+ xdg-open "http://127.0.0.1:8080"
 else 
 echo "Please setup the project first using scripts/linux_setup.sh"
 exit 0
 fi 
-
-#Run Server using LLAMA
-python3 clean-ui.py --llama&
-
-#Open browser?
-xdg-open "http://127.0.0.1:8080"
 
 exit 0
