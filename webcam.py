4a584ae158a229ab5e4122292cb2c815fe5bf1e9
Ammar Qammaz <ammarkov@gmail.com>
2024-10-17 09:42:00 +0300

    use localhost as default IP

--- webcam.py
+++ webcam.py
@@ -8,7 +8,7 @@ import tempfile
 #python3 -m pip install opencv-python gradio-client
 
 # Replace with the actual server URL if different
-ip   = "139.91.90.47"
+ip   = "127.0.0.1"
 port = "8080"
 
 # Define the user prompt (caption)
